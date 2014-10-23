f5a73923f5754a95ae06344ce45616cc62827ed9
alekseyl1992 <alekseyl@list.ru>
2014-10-23 20:09:53 +0400

    #BH-12 In Progress Prototype ready, Loader required, Buttons styling required

--- app/src/main/java/org/techteam/bashhappens/fragments/PostsListFragment.java
+++ app/src/main/java/org/techteam/bashhappens/fragments/PostsListFragment.java
@@ -1,110 +1,102 @@
 package org.techteam.bashhappens.fragments;
 
 import android.app.Activity;
+import android.app.ListFragment;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import org.techteam.bashhappens.R;
+import org.techteam.bashhappens.content.ContentEntry;
+import org.techteam.bashhappens.content.ContentFactory;
+import org.techteam.bashhappens.content.ContentList;
+import org.techteam.bashhappens.content.ContentSource;
+import org.techteam.bashhappens.content.FeedOverflowException;
+import org.techteam.bashhappens.content.bashorg.BashOrgEntry;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PostsListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PostsListFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
-public class PostsListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PostsListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PostsListFragment newInstance(String param1, String param2) {
-        PostsListFragment fragment = new PostsListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-    public PostsListFragment() {
-        // Required empty public constructor
-    }
+public class PostsListFragment extends ListFragment {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        String li = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+//        BashOrgEntry entry1 = new BashOrgEntry("#12345", "23.10.2014", li);
+//        BashOrgEntry entry2 = new BashOrgEntry("#12345", "23.10.2014", li);
+//        BashOrgEntry entry3 = new BashOrgEntry("#12345", "23.10.2014", li);
+//
+//        List<BashOrgEntry> list = new ArrayList<BashOrgEntry>();
+//        list.add(entry1);
+//        list.add(entry2);
+//        list.add(entry3);
+
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ContentFactory factory = new ContentFactory(Locale.getDefault().toString());
+                    ContentSource content = factory.buildContent(ContentFactory.ContentSection.BASH_ORG_NEWEST);
+                    final ContentList<?> list = content.retrieveNextList();
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            setListAdapter(new BashOrgListAdapter((List<BashOrgEntry>) list.getEntries()));
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (FeedOverflowException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        th.start();
+
+    }
+
+    private class BashOrgListAdapter extends ArrayAdapter<BashOrgEntry> {
+
+        public BashOrgListAdapter(List<BashOrgEntry> objects) {
+            super(getActivity(), 0, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.bashorg_list_entry, parent, false);
+                viewHolder = new ViewHolder();
+
+                viewHolder.id = (TextView) convertView.findViewById(R.id.post_id);
+                viewHolder.date = (TextView) convertView.findViewById(R.id.post_date);
+                viewHolder.text = (TextView) convertView.findViewById(R.id.post_text);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            viewHolder.id.setText(getItem(position).getId());
+            viewHolder.date.setText(getItem(position).getCreationDate());
+            viewHolder.text.setText(getItem(position).getText());
+            return convertView;
+        }
+
+        private class ViewHolder {
+            public TextView id;
+            public TextView date;
+            public TextView text;
         }
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_posts_list, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
-
 }
