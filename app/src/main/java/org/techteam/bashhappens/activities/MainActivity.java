7ea403b42dad5ae155ec2402e705bc3d86492593
alekseyl1992 <alekseyl@list.ru>
2014-10-26 15:06:24 +0300

    #BH-8 In Progress Bash/IT selection in side bar implemented

--- app/src/main/java/org/techteam/bashhappens/activities/MainActivity.java
+++ app/src/main/java/org/techteam/bashhappens/activities/MainActivity.java
@@ -1,15 +1,22 @@
 package org.techteam.bashhappens.activities;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.techteam.bashhappens.R;
 import org.techteam.bashhappens.content.ContentFactory;
 import org.techteam.bashhappens.content.ContentSource;
-import org.techteam.bashhappens.fragments.PostActionsFragment;
 import org.techteam.bashhappens.fragments.PostsListFragment;
+import org.techteam.bashhappens.util.Toaster;
 
 import java.util.Locale;
 
@@ -19,46 +26,75 @@ public class MainActivity extends Activity {
     ContentFactory factory = new ContentFactory(Locale.getDefault().toString());
     ContentSource content = null;
 
+    private String[] sections;
+    private DrawerLayout mDrawerLayout;
+    //private ActionBarDrawerToggle mDrawerToggle;
+    private ListView mDrawerList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(android.R.id.content, new PostsListFragment()).commit();
+                    .add(R.id.content_frame, new PostsListFragment()).commit();
         }
-        /*Button b = (Button) findViewById(R.id.button_test);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Thread th = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            content = factory.buildContent(ContentFactory.ContentSection.BASH_ORG_NEWEST);
-                            ContentList<?> list = content.retrieveNextList();
-                            for (ContentEntry e : list.getEntries()) {
-                                switch (e.getContentType()) {
-                                    case BASH_ORG:
-                                        BashOrgEntry entry = e.toBashOrgEntry();
-                                        break;
-                                    case IT_HAPPENS:
-                                        // BashOrgEntry entry = e.toBashOrgEntry();
-                                        break;
-                                }
-                            }
-                            System.out.println("hi");
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } catch (FeedOverflowException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-                th.start();
-            }
-        });*/
+
+        sections = getSections();//getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, sections));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    private String[] getSections() {
+        //TODO: may be move it to content package, maybe to resources, maybe both, maybe not
+
+        String[] sections = new String[] {
+                "Bash.org", "ITHappens"
+        };
+
+        return sections;
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Create a new fragment and specify the planet to show based on position
+        /*Fragment fragment = new PlanetFragment();
+        Bundle args = new Bundle();
+        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+        fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();*/
+
+        Toaster.toast(getBaseContext(), sections[position]);
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(sections[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        //mTitle = title;
+        getActionBar().setTitle(title);
     }
 
     @Override
